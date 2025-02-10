3adce39e216701fc95d63b49994b804736cb87a5
RundownRhino <52856631+RundownRhino@users.noreply.github.com>
2025-02-10 20:33:06 +0300

    Start a scan on other finishes, too

--- Code/AutoScanPatcher.cs
+++ Code/AutoScanPatcher.cs
@@ -17,11 +17,18 @@ public sealed class AutoScanPatcher
 [HarmonyPatch(typeof(ScanSys))]
 class ScanSysPatch
 {
-    [HarmonyPatch("OnScanDeep")]
+    [HarmonyPatch("OnScanDone")]
     [HarmonyPostfix]
-    static void OnScanDeepPatch(ScanSys __instance)
+    static void OnScanDonePatch(ScanSys __instance)
     {
-        if (__instance.FoundSO != null)
+        if (__instance.ScanProgress != 1f)
+        {
+            // then the scan isn't actually done
+            D.Warn("AutoScan: OnScanDone called but ScanProgress is {0}, ignoring.", __instance.ScanProgress);
+            return;
+        }
+        // We start a new deep scan if another kind finished (to not waste time) OR if the a deep scan finishes successfully
+        if (__instance.Mode != ScanMode.Deep || __instance.FoundSO != null)
         {
             // Is that the right way to get the LogSys instance?..
             __instance.S.Sys.Log.AddLine("[AutoScan] Starting new Deep Scan.");
@@ -30,7 +37,7 @@ class ScanSysPatch
         }
         else
         {
-            // Otherwise the scan was saturated.
+            // Otherwise a deep scan just failed, so don't start a new one.
             __instance.S.Sys.Log.AddLine("[AutoScan] Not starting a new Deep Scan as this one seems to have failed.");
         }
 
